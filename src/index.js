e12a1388f484773411989d879d182d4f48f5cbe3
Dragos <dragos360@live.com>
2024-04-14 00:39:31 +0300

    component name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import calendar from "./components/calendarComponent.vue";
 
 export default {
-    install(app, options) {
-        app.component('vue-events-calendar', calendar)
+    install(app) {
+        app.component('EventsCalendar', calendar)
     }
 }
